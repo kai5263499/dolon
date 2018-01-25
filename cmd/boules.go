c1ab48297d5bda1f1dcf57d6407f5141d3cc4108
Wes Widner <kai5263499@gmail.com>
2018-01-25 13:20:32 -0500

    wip

--- cmd/boules.go
+++ cmd/boules.go
@@ -1,39 +1,49 @@
 package main
 
 import (
-	"fmt"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
+	"github.com/google/gopacket/layers"
+	"github.com/Sirupsen/logrus"
+	"fmt"
+	"flag"
+)
+
+var (
+	port uint
 )
 
 func main() {
+
+	// TODO: Make device configurable
+
+	flag.UintVar(&port, "port", 80, "port")
+
+	flag.Parse()
+
+	logrus.SetLevel(logrus.DebugLevel)
+
 	handle, err := pcap.OpenLive("en0", 1600, true, pcap.BlockForever)
 	if err != nil {
 		panic(err)
 	}
 
-	bpfInstructions := []pcap.BPFInstruction{
-		{ 0x28, 0, 0, 0x0000000c },
-		{ 0x15, 0, 10, 0x00000800 },
-		{ 0x30, 0, 0, 0x00000017 },
-		{ 0x15, 0, 8, 0x00000006 },
-		{ 0x28, 0, 0, 0x00000014 },
-		{ 0x45, 6, 0, 0x00001fff },
-		{ 0xb1, 0, 0, 0x0000000e },
-		{ 0x48, 0, 0, 0x0000000e },
-		{ 0x15, 2, 0, 0x00001f40 },
-		{ 0x48, 0, 0, 0x00000010 },
-		{ 0x15, 0, 1, 0x00001f40 },
-		{ 0x6, 0, 0, 0x00040000 },
-		{ 0x6, 0, 0, 0x00000000 },
-	}
+	captureCmd := fmt.Sprintf("tcp and dst port %d", port)
+	captureCmd = "arp"
 
-	if err := handle.SetBPFInstructionFilter(bpfInstructions); err != nil {
+	logrus.Debugf("captureCmd=%s", captureCmd)
+
+	if err := handle.SetBPFFilter(captureCmd); err != nil {
 		panic(err)
 	}
 
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
-		fmt.Printf("%s", packet.Dump())
+		logrus.Debugf("Packet data: packet=%#v", packet)
+		for _, layer := range packet.Layers() {
+			if layer.LayerType() == layers.LayerTypeTCP && len(layer.LayerPayload()) > 0{
+				logrus.Infof("TCP layer payload: %#v", string(layer.LayerPayload()))
+			}
+		}
 	}
 }

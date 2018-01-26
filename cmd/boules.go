eb692f90c34a8ab26b26bf5d32a1435bd0e58de9
Wes Widner <wes.widner@crowdstrike.com>
2018-01-26 18:21:40 -0500

    more wip

--- cmd/boules.go
+++ cmd/boules.go
@@ -1,48 +1,46 @@
 package main
 
 import (
-	"github.com/google/gopacket"
-	"github.com/google/gopacket/pcap"
-	"github.com/google/gopacket/layers"
-	"github.com/Sirupsen/logrus"
-	"fmt"
 	"flag"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
 )
 
 var (
-	port uint
+	device    string
+	bpfFilter string
 )
 
 func main() {
 
-	// TODO: Make device configurable
-
-	flag.UintVar(&port, "port", 80, "port")
+	flag.StringVar(&device, "device", "eth0", "device")
+	flag.StringVar(&bpfFilter, "filter", "tcp and dst port ", "filter")
 
 	flag.Parse()
 
 	logrus.SetLevel(logrus.DebugLevel)
 
-	handle, err := pcap.OpenLive("en0", 1600, true, pcap.BlockForever)
+	handle, err := pcap.OpenLive(device, 1600, true, pcap.BlockForever)
 	if err != nil {
 		panic(err)
 	}
 
-	captureCmd := fmt.Sprintf("tcp and dst port %d", port)
-	captureCmd = "arp"
+	logrus.Debugf("device=%s bpfFilter=%s", device, bpfFilter)
 
-	logrus.Debugf("captureCmd=%s", captureCmd)
-
-	if err := handle.SetBPFFilter(captureCmd); err != nil {
+	if err := handle.SetBPFFilter(bpfFilter); err != nil {
 		panic(err)
 	}
 
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+
 	for packet := range packetSource.Packets() {
-		logrus.Debugf("Packet data: packet=%#v", packet)
 		for _, layer := range packet.Layers() {
-			if layer.LayerType() == layers.LayerTypeTCP && len(layer.LayerPayload()) > 0{
-				logrus.Infof("TCP layer payload: %#v", string(layer.LayerPayload()))
+			if layer.LayerType() == layers.LayerTypeTCP {
+				layerPayload := string(layer.LayerPayload())
+				logrus.Infof("TCP layer payload: [%s]", layerPayload)
 			}
 		}
 	}
